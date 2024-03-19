d38cbdb1e686b46fcbb9ebe09040f29d5234d5ff
AmidamaruPWNZ <nikita14915@gmail.com>
2024-03-19 22:33:30 +0500

    n

--- main.py
+++ main.py
@@ -1,43 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import csv
-import undetected_chromedriver as uc
+import requests
+import pandas as pd
 
-def scrape_elon_tweets():
-    driver = uc.Chrome()
+twitter_data = []
 
-    url = "https://twitter.com/elonmusk"
+payload = {
+    'api_key': '83743cec7f796a37b4f6233ec3e1ed1c',
+    'query': 'elonmusk',
+    'num': '12',
+}
 
-    driver.get(url)
+response = requests.get(
+    'https://api.scraperapi.com/structured/twitter/search', params=payload
+)
 
-    driver.implicitly_wait(10)
+data = response.json()
 
-    tweets = driver.find_element(By.XPATH, '//div[@data-testid="tweet"]')
+print(data.keys())
+print(data)
 
-    tweet_texts = []
+all_tweets = data['organic_results']
+for tweet in all_tweets:
+    twitter_data.append(tweet)
 
-    for tweet in tweets[:10]:
-        try:
-            tweet_text = tweet.find_element_by_xpath('.//div[@lang="en"]').text
-            tweet_texts.append(tweet_text)
-        except:
-            pass
-
-    driver.quit()
-
-    return tweet_texts
-
-def save_to_csv(tweet_texts):
-    with open('elon_tweets.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Tweet'])
-        for tweet in tweet_texts:
-            writer.writerow([tweet])
-
-def main():
-    tweet_texts = scrape_elon_tweets()
-    save_to_csv(tweet_texts)
-    print("Elon Musk's recent tweets have been saved to 'elon_tweets.csv'")
-
-if __name__ == "__main__":
-    main()
+df = pd.DataFrame(twitter_data)
+df.to_csv('ElonTweets.csv', index=False)
+print('Done')
